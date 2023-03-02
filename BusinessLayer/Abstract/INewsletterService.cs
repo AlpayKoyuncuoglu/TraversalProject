ad5adf5f45f635759b3b983678686ffeeb832b8d
akoyuncuoglu <akoyuncuoglu@PC-KOYUNCUOGLU>
2023-03-02 10:21:16 +0300

    BusinessLayer works

--- BusinessLayer/Abstract/INewsletterService.cs
+++ BusinessLayer/Abstract/INewsletterService.cs
@@ -5,9 +5,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DataAccessLayer.Abstract
+namespace BusinessLayer.Abstract
 {
-    public interface IAboutSecond:IGenericDal<AboutSecond>
+    public interface INewsletterService : IGenericService<Newsletter>
     {
     }
 }
